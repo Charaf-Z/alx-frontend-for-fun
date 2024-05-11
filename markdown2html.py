c82f2edc2cbb533a1bc615a35339e77abcd74a69
Charaf-Z <charafomarzitouni@gmail.com>
2024-05-11 20:27:31 +0100

    another update to the code

--- markdown2html.py
+++ markdown2html.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     with open(argv[1]) as read:
         with open(argv[2], "w") as html:
-            ul, ol, p = False, False, False
+            start_ul, start_ol, pr = False, False, False
             for line in read:
                 line = line.replace("**", "<b>", 1)
                 line = line.replace("**", "</b>", 1)
@@ -32,62 +32,63 @@ if __name__ == "__main__":
                         md5_item[0], md5(md5_inside[0].encode()).hexdigest()
                     )
 
-                remove_c = findall(r"\(\(.+?\)\)", line)
-                remove_c_inside = findall(r"\(\((.+?)\)\)", line)
-                if remove_c:
-                    remove_c_inside = "".join(
-                        c for c in remove_c_inside[0] if c not in "Cc"
+                c_string = findall(r"\(\(.+?\)\)", line)
+                c_content = findall(r"\(\((.+?)\)\)", line)
+                if c_string:
+                    c_content = "".join(
+                        c for c in c_content[0] if c not in "Cc"
                     )
-                    line = line.replace(remove_c[0], remove_c_inside)
+                    line = line.replace(c_string[0], c_content)
 
                 length = len(line)
                 header = line.lstrip("#")
                 header_level = length - len(header)
+                ul = line.lstrip("-")
+                ul_nbr = length - len(ul)
+                ol = line.lstrip("*")
+                ol_nbr = length - len(ol)
                 if 1 <= header_level <= 6:
-                    line = "<h{0}>{1}</h{0}>\n".format(
-                        header_level, header.strip()
+                    line = (
+                        "<h{}>".format(header_level)
+                        + header.strip()
+                        + "</h{}>\n".format(header_level)
                     )
 
-                ul_li = line.lstrip("-")
-                ul_nbr = length - len(ul_li)
                 if ul_nbr:
-                    if not ul:
+                    if not start_ul:
                         html.write("<ul>\n")
-                        ul = True
-                    line = "<li>{}</li>\n".format(ul_li.strip())
-                if ul and not ul_nbr:
+                        start_ul = True
+                    line = "<li>" + ul.strip() + "</li>\n"
+                if start_ul and not ul_nbr:
                     html.write("</ul>\n")
-                    ul = False
+                    start_ul = False
 
-                ol_li = line.lstrip("*")
-                ol_nbr = length - len(ol_li)
                 if ol_nbr:
-                    if not ol:
+                    if not start_ol:
                         html.write("<ol>\n")
-                        ol = True
-                    line = "<li>{}</li>".format(ol_li.strip())
-                if ol and not ol_nbr:
+                        start_ol = True
+                    line = "<li>" + ol.strip() + "</li>\n"
+                if start_ol and not ol_nbr:
                     html.write("</ol>\n")
-                    ol = False
+                    start_ol = False
 
-                if not (header_level or ol or ul):
-                    if not p and length > 1:
+                if not (header_level or start_ul or start_ol):
+                    if not pr and length > 1:
                         html.write("<p>\n")
-                        p = True
+                        pr = True
                     elif length > 1:
                         html.write("<br/>\n")
-                    elif p:
+                    elif pr:
                         html.write("</p>\n")
-                        p = False
+                        pr = False
 
                 if length > 1:
                     html.write(line)
 
-            if ul:
+            if start_ul:
                 html.write("</ul>\n")
-            if ol:
+            if start_ol:
                 html.write("</ol>\n")
-            if p:
+            if pr:
                 html.write("</p>\n")
-
     exit(0)
